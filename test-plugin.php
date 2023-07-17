2a0878d817d2f3d551228f47fc770cd21df8569a
Gordon Yee <gonyee@ucsc.edu>
2023-07-17 11:41:42 -0700

    first commit

---
+++ test-plugin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Plugin Name: test-plugin
+* Plugin URI: https://dev-ai-college-assists.pantheonsite.io/
+* Description: Test.
+* Version: 0.1
+* Author: Gordon and Claire
+* Author URI: https://dev-ai-college-assists.pantheonsite.io/
+**/
