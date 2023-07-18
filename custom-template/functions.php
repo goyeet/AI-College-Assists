35629e7348d992cb09f8480023a30b0b93807038
CLARISSA CHEN <clarissachen5@gmail.com>
2023-07-18 10:24:23 -0700

    successfully added title to template

---
+++ custom-template/functions.php
@@ -0,0 +1,8 @@
+<?php 
+
+function custom_theme_enqueue_styles() {
+    wp_enqueue_style('custom-style', get_template_directory_uri() . '/style.css', array(), '1.0', 'all');
+}
+add_action('wp_enqueue_scripts', 'custom_theme_enqueue_styles');
+
+?>
--- custom-template/index.php
+++ custom-template/index.php
@@ -1,25 +1,15 @@
-/*Version: 0.1
+<?php 
+/*
+
+Version: 0.1
 * Author: Gordon and Clarissa
 * Author URI: https://dev-ai-college-assists.pantheonsite.io/
 */
 
-<h1> Hello world! </h1>
+?>
+<h1>Hello Everyone </h1>
 
-
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-  <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="profile" href="https://gmpg.org/xfn/11">
-    <?php wp_head(); ?>
-  </head>
-
-  <header class="site-header">
-	<p class="site-title">
-		<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-			<?php bloginfo( 'name' ); ?>
-		</a>
-	</p>
-	<p class="site-description"><?php bloginfo( 'description' ); ?></p>
-</header><!-- .site-header -->
+<?php get_template_part( 'header' ); ?>
+<?php 
+get_footer();
+?>
