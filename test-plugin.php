77320c92082115bd9622064fb1b49e8abd3fa9ae
Gordon Yee <gonyee@ucsc.edu>
2023-07-18 09:17:48 -0700

    cleaned up repo and updated plugin and page template files

--- test-plugin.php
+++ test-plugin.php
@@ -8,24 +8,45 @@
 * Author URI: https://dev-ai-college-assists.pantheonsite.io/
 **/
 
-$wp_user_id = 309;
-$wp_user_key = ''; // deleted key for security
-
-$args = array($wp_user_id, $wp_user_key);
-
-$request = wp_remote_get( 'https://haily.aiexosphere.com/get_user_credits/' , $args);
-
-if ( is_wp_error( $request ) ) {
-	return false;
+// Register the plugin settings
+function gig_register_settings() {
+    add_option( 'gig_user_key', '' ); // Initialize the option with an empty value
+    register_setting( 'gig_settings', 'gig_user_key' );
+	add_option( 'gig_user_id', '' ); // Initialize the option with an empty value
+    register_setting( 'gig_settings', 'gig_user_id' );
 }
+add_action( 'admin_init', 'gig_register_settings' );
 
-// print_r($request);
+// Add the plugin settings menu
+function gig_add_settings_page() {
+    add_options_page( 'GIG Settings', 'GIG', 'manage_options', 'gig_settings', 'gig_settings_page' );
+}
+add_action( 'admin_menu', 'gig_add_settings_page' );
 
-$body = 'test ' . wp_remote_retrieve_body( $request );
-
-// print_r($body);
-
-$data = json_decode( $body );
-
-// print_r($data);
-
+// Render the plugin settings page
+function gig_settings_page() {
+    ?>
+    <div class="wrap">
+        <h2>GIG Settings</h2>
+        <form method="post" action="options.php">
+            <?php settings_fields( 'gig_settings' ); ?>
+            <?php do_settings_sections( 'gig_settings' ); ?>
+            <table class="form-table">
+                <tr valign="top">
+                    <th scope="row">User ID</th>
+                    <td>
+                        <input type="text" name="gig_user_id" value="<?php echo esc_attr( get_option( 'gig_user_id' ) ); ?>" />
+                    </td>
+                </tr>
+				<tr valign="top">
+                    <th scope="row">User Key</th>
+                    <td>
+                        <input type="text" name="gig_user_key" value="<?php echo esc_attr( get_option( 'gig_user_key' ) ); ?>" />
+                    </td>
+                </tr>
+            </table>
+            <?php submit_button(); ?>
+        </form>
+    </div>
+    <?php
+}
