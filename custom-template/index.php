75fc4ecb967b5cead0f35b817640e3eeedacbc60
CLARISSA CHEN <clarissachen5@gmail.com>
2023-07-18 09:35:08 -0700

    edits to index.php  Please enter the commit message for your changes. Lines starting

--- custom-template/index.php
+++ custom-template/index.php
@@ -4,3 +4,22 @@
 */
 
 <h1> Hello world! </h1>
+
+
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+  <head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="profile" href="https://gmpg.org/xfn/11">
+    <?php wp_head(); ?>
+  </head>
+
+  <header class="site-header">
+	<p class="site-title">
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+			<?php bloginfo( 'name' ); ?>
+		</a>
+	</p>
+	<p class="site-description"><?php bloginfo( 'description' ); ?></p>
+</header><!-- .site-header -->
