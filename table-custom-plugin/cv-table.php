e79fe51c1a81970f7bdaf3148c5f07e46e030fe6
CLARISSA CHEN <clarissachen5@gmail.com>
2023-08-03 19:11:27 -0700

    table parses cv input & collect user's entries
    Co-authored-by: Gordon Yee <goyeet@users.noreply.github.com>

--- table-custom-plugin/cv-table.php
+++ table-custom-plugin/cv-table.php
@@ -1,4 +1,4 @@
-<?php if (empty($table_data)) : ?>
+<?php if (empty($cv_form_entry_data)) : ?>
     <p>No data available.</p>
 <?php else : ?>
     
@@ -37,23 +37,61 @@
             <th>Non-school Accomplishments</th>
             <th>Passions</th>
             <th>Life Changing Moments</th>
-          
         </tr>
 
-        <?php foreach ($table_data as $row) : ?> 
-            <tr>
-                <td class="date"><?php echo esc_html($row['date']);?></td>
-                <td class="value"><?php echo esc_html($row['value']);?></td>
-                <td class="response-box">
-                    <div class="generated-response"></div>
-                </td>
-                <td class="button-cell">
-                    <button class="generate-button">Generate</button>
-                    <div class="loading-spinner hidden">
-                        <div class="loader"></div>
-                    </div>
-                </td>
-            </tr>
-        <?php endforeach; ?>
+        <?php
+            // filter out all results that aren't associated with user email
+            $entered_user_email = wp_get_current_user()->user_email; // get user email
+            print_r($entered_user_email);
+
+            $users_entry_ids = array();
+
+            // loop through data table and gather all entry ids associated with user email
+            foreach ($cv_form_entry_data as $row) {
+                print_r('value: '.$row['value']);
+                if ($row['value'] == $entered_user_email) { //if entered email equals value for that row
+                    // store entry id in array
+                    
+                    array_push($users_entry_ids, $row['entry_id']);
+                    print_r('pushing '.$row['entry_id'].' to array');
+                }
+            }
+
+            // user did not have any entries in data table
+            if (empty($users_entry_ids)) {
+                echo '<p>No data available. Please enter your CV information through the provided form and try again.</p>';
+                die;
+            }
+
+            print_r($users_entry_ids); // debugging purposes
+
+            // $rows_in_table = array();
+        ?>
+        <?php
+            // loop through data table and find all rows that are associated with user's entry ids
+            foreach ($cv_form_entry_data as $row) {
+                if (in_array($row['entry_id'], $users_entry_ids)) {?>
+                    <tr>
+                        <td class="entry-id"><?php echo esc_html($row['entry_id']);?></td>
+                        <td class="date"><?php echo esc_html($row['date']);?></td>
+                        <td class="value"><?php echo esc_html($row['value']);?></td>
+                    </tr>
+                
+                    <?php
+                    //create new table row
+                    //delete entry id from entry ids array
+                    /* $field_id = $row['field_id'];
+                    $field_value = $row['value'];
+                    $field_name = wpforms()->get('field', $field_id)->name; */
+
+                    
+                    /* if (in_array($row['entry_id']), $rows_in_table) {
+                        //add value to table
+                    } */
+                
+                //else (entry id is not in the array so it already has a row from the table)
+                }
+            }
+        ?>
     </table>
 <?php endif; ?>
